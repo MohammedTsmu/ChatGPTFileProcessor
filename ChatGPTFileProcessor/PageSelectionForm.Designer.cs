2cc08c5309ea85783b9054c77bf11b9544b00b2b
Mohammed <Mohammed@DESKTOP-SJUV98T>
2025-06-02 00:47:00 +0300

    ui+

--- ChatGPTFileProcessor/PageSelectionForm.Designer.cs
+++ ChatGPTFileProcessor/PageSelectionForm.Designer.cs
@@ -28,17 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PageSelectionForm));
             this.galleryControl1 = new DevExpress.XtraBars.Ribbon.GalleryControl();
             this.galleryControlClient1 = new DevExpress.XtraBars.Ribbon.GalleryControlClient();
             this.spinFrom = new DevExpress.XtraEditors.SpinEdit();
             this.spinTo = new DevExpress.XtraEditors.SpinEdit();
             this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.panel2 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.galleryControl1)).BeginInit();
             this.galleryControl1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.spinFrom.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.spinTo.Properties)).BeginInit();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // galleryControl1
@@ -47,7 +52,7 @@
             this.galleryControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.galleryControl1.Location = new System.Drawing.Point(0, 0);
             this.galleryControl1.Name = "galleryControl1";
-            this.galleryControl1.Size = new System.Drawing.Size(1062, 673);
+            this.galleryControl1.Size = new System.Drawing.Size(1062, 619);
             this.galleryControl1.TabIndex = 0;
             this.galleryControl1.Text = "galleryControl1";
             // 
@@ -55,48 +60,61 @@
             // 
             this.galleryControlClient1.GalleryControl = this.galleryControl1;
             this.galleryControlClient1.Location = new System.Drawing.Point(2, 2);
-            this.galleryControlClient1.Size = new System.Drawing.Size(1037, 669);
+            this.galleryControlClient1.Size = new System.Drawing.Size(1037, 615);
             // 
             // spinFrom
             // 
+            this.spinFrom.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.spinFrom.EditValue = new decimal(new int[] {
             0,
             0,
             0,
             0});
-            this.spinFrom.Location = new System.Drawing.Point(78, 3);
+            this.spinFrom.Location = new System.Drawing.Point(114, 11);
             this.spinFrom.Name = "spinFrom";
+            this.spinFrom.Properties.Appearance.Font = new System.Drawing.Font("LBC", 12F);
+            this.spinFrom.Properties.Appearance.Options.UseFont = true;
             this.spinFrom.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-            this.spinFrom.Size = new System.Drawing.Size(125, 24);
+            this.spinFrom.Size = new System.Drawing.Size(125, 32);
             this.spinFrom.TabIndex = 1;
             // 
             // spinTo
             // 
+            this.spinTo.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.spinTo.EditValue = new decimal(new int[] {
             0,
             0,
             0,
             0});
-            this.spinTo.Location = new System.Drawing.Point(226, 3);
+            this.spinTo.Location = new System.Drawing.Point(337, 11);
             this.spinTo.Name = "spinTo";
+            this.spinTo.Properties.Appearance.Font = new System.Drawing.Font("LBC", 12F);
+            this.spinTo.Properties.Appearance.Options.UseFont = true;
             this.spinTo.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-            this.spinTo.Size = new System.Drawing.Size(125, 24);
+            this.spinTo.Size = new System.Drawing.Size(125, 32);
             this.spinTo.TabIndex = 2;
             // 
             // simpleButton1
             // 
+            this.simpleButton1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.simpleButton1.Appearance.Font = new System.Drawing.Font("LBC", 12F);
+            this.simpleButton1.Appearance.Options.UseFont = true;
             this.simpleButton1.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.simpleButton1.Location = new System.Drawing.Point(376, 7);
+            this.simpleButton1.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("simpleButton1.ImageOptions.SvgImage")));
+            this.simpleButton1.ImageOptions.SvgImageSize = new System.Drawing.Size(25, 25);
+            this.simpleButton1.Location = new System.Drawing.Point(922, 10);
             this.simpleButton1.Name = "simpleButton1";
-            this.simpleButton1.Size = new System.Drawing.Size(94, 29);
+            this.simpleButton1.Size = new System.Drawing.Size(128, 35);
             this.simpleButton1.TabIndex = 3;
             this.simpleButton1.Text = "OK";
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.labelControl2);
+            this.panel1.Controls.Add(this.labelControl1);
             this.panel1.Controls.Add(this.spinFrom);
             this.panel1.Controls.Add(this.simpleButton1);
             this.panel1.Controls.Add(this.spinTo);
@@ -106,22 +124,62 @@
             this.panel1.Size = new System.Drawing.Size(1062, 54);
             this.panel1.TabIndex = 4;
             // 
+            // labelControl1
+            // 
+            this.labelControl1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("LBC", 12F);
+            this.labelControl1.Appearance.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Appearance.Options.UseForeColor = true;
+            this.labelControl1.Location = new System.Drawing.Point(12, 14);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(96, 26);
+            this.labelControl1.TabIndex = 4;
+            this.labelControl1.Text = "Start Page";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("LBC", 12F);
+            this.labelControl2.Appearance.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Appearance.Options.UseForeColor = true;
+            this.labelControl2.Location = new System.Drawing.Point(245, 14);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(86, 26);
+            this.labelControl2.TabIndex = 5;
+            this.labelControl2.Text = "End Page";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.galleryControl1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1062, 619);
+            this.panel2.TabIndex = 5;
+            // 
             // PageSelectionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 26F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1062, 673);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.galleryControl1);
             this.Font = new System.Drawing.Font("LBC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "PageSelectionForm";
-            this.Text = "PageSelectionForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pages Selection";
             ((System.ComponentModel.ISupportInitialize)(this.galleryControl1)).EndInit();
             this.galleryControl1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.spinFrom.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.spinTo.Properties)).EndInit();
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -134,5 +192,8 @@
         private DevExpress.XtraEditors.SpinEdit spinTo;
         private DevExpress.XtraEditors.SimpleButton simpleButton1;
         private System.Windows.Forms.Panel panel1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
