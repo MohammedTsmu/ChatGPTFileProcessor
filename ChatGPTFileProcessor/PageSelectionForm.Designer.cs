242b4fd1379cb02992a35b0622008c9a68f65cb9
MohammedTsmu <mohsat69@yahoo.com>
2025-12-22 16:46:31 +0300

    Improve UI layout and maximize window by default
    Form1 and PageSelectionForm now open maximized for better user experience. PageSelectionForm layout and controls were enlarged and repositioned for improved visibility and usability. StartPosition changed to CenterScreen. Unused resources and redundant designer code removed for cleanup.

--- ChatGPTFileProcessor/PageSelectionForm.Designer.cs
+++ ChatGPTFileProcessor/PageSelectionForm.Designer.cs
@@ -17,9 +17,6 @@
 
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PageSelectionForm));
-
             this.galleryControl1 = new DevExpress.XtraBars.Ribbon.GalleryControl();
             this.galleryControlClient1 = new DevExpress.XtraBars.Ribbon.GalleryControlClient();
             this.panelTop = new DevExpress.XtraEditors.PanelControl();
@@ -36,7 +33,6 @@
             this.btnOK = new DevExpress.XtraEditors.SimpleButton();
             this.btnCancel = new DevExpress.XtraEditors.SimpleButton();
             this.labelHelp = new DevExpress.XtraEditors.LabelControl();
-
             ((System.ComponentModel.ISupportInitialize)(this.galleryControl1)).BeginInit();
             this.galleryControl1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.panelTop)).BeginInit();
@@ -48,24 +44,23 @@
             ((System.ComponentModel.ISupportInitialize)(this.spinFrom.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.spinTo.Properties)).BeginInit();
             this.SuspendLayout();
-
             // 
             // galleryControl1
             // 
             this.galleryControl1.Controls.Add(this.galleryControlClient1);
             this.galleryControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.galleryControl1.Location = new System.Drawing.Point(0, 80);
+            this.galleryControl1.Location = new System.Drawing.Point(0, 98);
+            this.galleryControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.galleryControl1.Name = "galleryControl1";
-            this.galleryControl1.Size = new System.Drawing.Size(1000, 550);
+            this.galleryControl1.Size = new System.Drawing.Size(1167, 677);
             this.galleryControl1.TabIndex = 0;
-
             // 
             // galleryControlClient1
             // 
             this.galleryControlClient1.GalleryControl = this.galleryControl1;
             this.galleryControlClient1.Location = new System.Drawing.Point(2, 2);
-            this.galleryControlClient1.Size = new System.Drawing.Size(996, 546);
-
+            this.galleryControlClient1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.galleryControlClient1.Size = new System.Drawing.Size(1142, 673);
             // 
             // panelTop
             // 
@@ -74,10 +69,10 @@
             this.panelTop.Controls.Add(this.lblFileInfo);
             this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelTop.Name = "panelTop";
-            this.panelTop.Size = new System.Drawing.Size(1000, 80);
+            this.panelTop.Size = new System.Drawing.Size(1167, 98);
             this.panelTop.TabIndex = 1;
-
             // 
             // lblTitle
             // 
@@ -85,12 +80,12 @@
             this.lblTitle.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
             this.lblTitle.Appearance.Options.UseFont = true;
             this.lblTitle.Appearance.Options.UseForeColor = true;
-            this.lblTitle.Location = new System.Drawing.Point(20, 15);
+            this.lblTitle.Location = new System.Drawing.Point(23, 18);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(200, 30);
+            this.lblTitle.Size = new System.Drawing.Size(236, 37);
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Select Page Range";
-
             // 
             // lblFileInfo
             // 
@@ -98,12 +93,12 @@
             this.lblFileInfo.Appearance.ForeColor = System.Drawing.Color.Gray;
             this.lblFileInfo.Appearance.Options.UseFont = true;
             this.lblFileInfo.Appearance.Options.UseForeColor = true;
-            this.lblFileInfo.Location = new System.Drawing.Point(20, 50);
+            this.lblFileInfo.Location = new System.Drawing.Point(23, 62);
+            this.lblFileInfo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lblFileInfo.Name = "lblFileInfo";
-            this.lblFileInfo.Size = new System.Drawing.Size(150, 19);
+            this.lblFileInfo.Size = new System.Drawing.Size(107, 23);
             this.lblFileInfo.TabIndex = 1;
             this.lblFileInfo.Text = "No file loaded";
-
             // 
             // panelBottom
             // 
@@ -115,11 +110,11 @@
             this.panelBottom.Controls.Add(this.btnCancel);
             this.panelBottom.Controls.Add(this.labelHelp);
             this.panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelBottom.Location = new System.Drawing.Point(0, 630);
+            this.panelBottom.Location = new System.Drawing.Point(0, 775);
+            this.panelBottom.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelBottom.Name = "panelBottom";
-            this.panelBottom.Size = new System.Drawing.Size(1000, 120);
+            this.panelBottom.Size = new System.Drawing.Size(1167, 148);
             this.panelBottom.TabIndex = 2;
-
             // 
             // groupRange
             // 
@@ -129,28 +124,33 @@
             this.groupRange.Controls.Add(this.spinFrom);
             this.groupRange.Controls.Add(this.labelTo);
             this.groupRange.Controls.Add(this.spinTo);
-            this.groupRange.Location = new System.Drawing.Point(20, 10);
+            this.groupRange.Location = new System.Drawing.Point(23, 12);
+            this.groupRange.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupRange.Name = "groupRange";
-            this.groupRange.Size = new System.Drawing.Size(350, 100);
+            this.groupRange.Size = new System.Drawing.Size(408, 123);
             this.groupRange.TabIndex = 0;
             this.groupRange.Text = "Page Range";
-
             // 
             // labelFrom
             // 
             this.labelFrom.Appearance.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.labelFrom.Appearance.Options.UseFont = true;
-            this.labelFrom.Location = new System.Drawing.Point(15, 40);
+            this.labelFrom.Location = new System.Drawing.Point(18, 49);
+            this.labelFrom.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.labelFrom.Name = "labelFrom";
-            this.labelFrom.Size = new System.Drawing.Size(30, 15);
+            this.labelFrom.Size = new System.Drawing.Size(37, 20);
             this.labelFrom.TabIndex = 0;
             this.labelFrom.Text = "From:";
-
             // 
             // spinFrom
             // 
-            this.spinFrom.EditValue = new decimal(new int[] { 1, 0, 0, 0 });
-            this.spinFrom.Location = new System.Drawing.Point(55, 37);
+            this.spinFrom.EditValue = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.spinFrom.Location = new System.Drawing.Point(64, 46);
+            this.spinFrom.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.spinFrom.Name = "spinFrom";
             this.spinFrom.Properties.Appearance.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.spinFrom.Properties.Appearance.Options.UseFont = true;
@@ -158,27 +158,39 @@
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
             this.spinFrom.Properties.IsFloatValue = false;
             this.spinFrom.Properties.Mask.EditMask = "N00";
-            this.spinFrom.Properties.MaxValue = new decimal(new int[] { 9999, 0, 0, 0 });
-            this.spinFrom.Properties.MinValue = new decimal(new int[] { 1, 0, 0, 0 });
-            this.spinFrom.Size = new System.Drawing.Size(100, 24);
+            this.spinFrom.Properties.MaxValue = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.spinFrom.Properties.MinValue = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.spinFrom.Size = new System.Drawing.Size(117, 30);
             this.spinFrom.TabIndex = 1;
-
             // 
             // labelTo
             // 
             this.labelTo.Appearance.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.labelTo.Appearance.Options.UseFont = true;
-            this.labelTo.Location = new System.Drawing.Point(180, 40);
+            this.labelTo.Location = new System.Drawing.Point(210, 49);
+            this.labelTo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.labelTo.Name = "labelTo";
-            this.labelTo.Size = new System.Drawing.Size(16, 15);
+            this.labelTo.Size = new System.Drawing.Size(20, 20);
             this.labelTo.TabIndex = 2;
             this.labelTo.Text = "To:";
-
             // 
             // spinTo
             // 
-            this.spinTo.EditValue = new decimal(new int[] { 1, 0, 0, 0 });
-            this.spinTo.Location = new System.Drawing.Point(210, 37);
+            this.spinTo.EditValue = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.spinTo.Location = new System.Drawing.Point(245, 46);
+            this.spinTo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.spinTo.Name = "spinTo";
             this.spinTo.Properties.Appearance.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.spinTo.Properties.Appearance.Options.UseFont = true;
@@ -186,35 +198,42 @@
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
             this.spinTo.Properties.IsFloatValue = false;
             this.spinTo.Properties.Mask.EditMask = "N00";
-            this.spinTo.Properties.MaxValue = new decimal(new int[] { 9999, 0, 0, 0 });
-            this.spinTo.Properties.MinValue = new decimal(new int[] { 1, 0, 0, 0 });
-            this.spinTo.Size = new System.Drawing.Size(100, 24);
+            this.spinTo.Properties.MaxValue = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.spinTo.Properties.MinValue = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.spinTo.Size = new System.Drawing.Size(117, 30);
             this.spinTo.TabIndex = 3;
-
             // 
             // btnSelectAll
             // 
             this.btnSelectAll.Appearance.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.btnSelectAll.Appearance.Options.UseFont = true;
-            this.btnSelectAll.Location = new System.Drawing.Point(390, 30);
+            this.btnSelectAll.Location = new System.Drawing.Point(455, 37);
+            this.btnSelectAll.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSelectAll.Name = "btnSelectAll";
-            this.btnSelectAll.Size = new System.Drawing.Size(120, 35);
+            this.btnSelectAll.Size = new System.Drawing.Size(140, 43);
             this.btnSelectAll.TabIndex = 1;
             this.btnSelectAll.Text = "Select All (Ctrl+A)";
             this.btnSelectAll.Click += new System.EventHandler(this.BtnSelectAll_Click);
-
             // 
             // btnClear
             // 
             this.btnClear.Appearance.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.btnClear.Appearance.Options.UseFont = true;
-            this.btnClear.Location = new System.Drawing.Point(520, 30);
+            this.btnClear.Location = new System.Drawing.Point(607, 37);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(120, 35);
+            this.btnClear.Size = new System.Drawing.Size(140, 43);
             this.btnClear.TabIndex = 2;
             this.btnClear.Text = "Clear (Esc)";
             this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
-
             // 
             // btnOK
             // 
@@ -225,24 +244,24 @@
             this.btnOK.Appearance.Options.UseFont = true;
             this.btnOK.Appearance.Options.UseForeColor = true;
             this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnOK.Location = new System.Drawing.Point(780, 30);
+            this.btnOK.Location = new System.Drawing.Point(910, 37);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(100, 40);
+            this.btnOK.Size = new System.Drawing.Size(117, 49);
             this.btnOK.TabIndex = 3;
             this.btnOK.Text = "OK";
-
             // 
             // btnCancel
             // 
             this.btnCancel.Appearance.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.btnCancel.Appearance.Options.UseFont = true;
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(890, 30);
+            this.btnCancel.Location = new System.Drawing.Point(1038, 37);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(100, 40);
+            this.btnCancel.Size = new System.Drawing.Size(117, 49);
             this.btnCancel.TabIndex = 4;
             this.btnCancel.Text = "Cancel";
-
             // 
             // labelHelp
             // 
@@ -250,28 +269,30 @@
             this.labelHelp.Appearance.ForeColor = System.Drawing.Color.Gray;
             this.labelHelp.Appearance.Options.UseFont = true;
             this.labelHelp.Appearance.Options.UseForeColor = true;
-            this.labelHelp.Location = new System.Drawing.Point(20, 85);
+            this.labelHelp.Location = new System.Drawing.Point(23, 105);
+            this.labelHelp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.labelHelp.Name = "labelHelp";
-            this.labelHelp.Size = new System.Drawing.Size(600, 13);
+            this.labelHelp.Size = new System.Drawing.Size(694, 19);
             this.labelHelp.TabIndex = 5;
-            this.labelHelp.Text = "💡 Tip: Click thumbnails to set range, Double-click to preview, Use arrow keys to navigate, Press Enter to zoom";
-
+            this.labelHelp.Text = "💡 Tip: Click thumbnails to set range, Double-click to preview, Use arrow keys to" +
+    " navigate, Press Enter to zoom";
             // 
             // PageSelectionForm
             // 
             this.AcceptButton = this.btnOK;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(1000, 750);
+            this.ClientSize = new System.Drawing.Size(1167, 923);
             this.Controls.Add(this.galleryControl1);
             this.Controls.Add(this.panelTop);
             this.Controls.Add(this.panelBottom);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MinimumSize = new System.Drawing.Size(800, 600);
             this.Name = "PageSelectionForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Select Pages";
-
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.galleryControl1)).EndInit();
             this.galleryControl1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.panelTop)).EndInit();
@@ -286,6 +307,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.spinFrom.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.spinTo.Properties)).EndInit();
             this.ResumeLayout(false);
+
         }
 
         #endregion
